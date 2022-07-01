3713f4e8c31b7db597b6e6cbc4ce70b2ba5bb4d5
PlumStream24 <sigid.iqbal123@gmail.com>
2022-07-01 10:40:38 +0700

    update on home screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card'
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { MatCardModule } from '@angular/material/card'
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Forms {
+  name: string;
+  creator: string;
+  date: Date;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +16,25 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  unfilledForms: Forms[] = [
+    {
+      name: 'Survey 1',
+      creator: 'anon',
+      date: new Date('1/1/16'),
+    },
+    {
+      name: 'Survey 2',
+      creator: 'anon',
+      date: new Date('1/17/16'),
+    }
+  ];
+
+  filledForms: Forms[] = [];
+
+  openForm() {
+
   }
 
 }
